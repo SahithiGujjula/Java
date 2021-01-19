c220344dbe22638b338acddb3cefe8df6e9bf6d2
SahithiGujjula <70298866+SahithiGujjula@users.noreply.github.com>
2021-01-18 19:36:25 -0600

    Update JewelsAndStones.java

--- JewelsAndStones.java
+++ JewelsAndStones.java
@@ -4,10 +4,8 @@ public class JewelsAndStones {
         public int numJewelsInStones(String jewels, String stones) {
             int count = 0;
             for(int i=0; i<stones.length(); i++){
-                char stone = stones.charAt(i);
                 for(int j=0; j<jewels.length(); j++){
-                    char jewel = jewels.charAt(j);
-                    if (stone == jewel){
+                    if (stones.charAt(i) == jewels.charAt(j)){
                         count++;
                     }
                 }
